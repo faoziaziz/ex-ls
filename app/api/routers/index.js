33a823ffc9d575deac86033f4e53912dfd2a46a5
Aziz Faozi <admin@labseni.com>
2019-10-06 23:13:00 +0700

    but still problem with the chor

--- app/api/routers/index.js
+++ app/api/routers/index.js
@@ -3,16 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main/index');
+  res.render('main/index', {
+    title: "LABSENI"
+  });
 });
 
 router.get('/about', function(req, res, next){
-  res.render('main/index');
+  res.render('main/index'), {
+    title: "LABSENI"
+  };
 
 });
 
 router.get('/product', function(req, res, next){
-  res.render('main/index');
+  res.render('main/index', {
+    title: "LABSENI"
+  });
 
 });
 module.exports = router;
